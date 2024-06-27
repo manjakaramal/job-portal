d1b0b0b81615629e5807bd51fe85939aa5213a31
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-06-28 01:24:19 +0530

    pwa added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+import { default as withPWA } from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
+
+// PWA configuration
+const withPWAConfig = withPWA({
+    dest: "public", // Directory where the service worker will be generated
+    disable: false, // Ensure that PWA features are enabled
+    cacheOnFrontEndNav: true, // Enable additional route caching when users navigate through pages with next/link
+    aggressiveFrontEndNavCaching: true, // Cache every <link rel="stylesheet" /> and <script /> on frontend navigation. Requires cacheOnFrontEndNav to be enabled
+    reloadOnOnline: true, // Reload the app when it has gone back online
+    workboxOptions: {
+        disableDevLogs: true // Disable Workbox logs in development mode for cleaner console output
+    },
+    cacheStartUrl: true, // Cache the start URL for faster initial load and offline availability
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWAConfig(nextConfig);
